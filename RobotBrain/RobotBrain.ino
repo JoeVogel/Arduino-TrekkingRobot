46711c28a1daad3713ad90ddab7fbdd11cf955e2
JoeVogel <joe_vogel@live.com>
2014-07-07 19:24:00 -0300

    Add compass functions

--- RobotBrain/RobotBrain.ino
+++ RobotBrain/RobotBrain.ino
@@ -1,1 +1,44 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_HMC5883_U.h>
 
+/* Assign a unique ID to this sensor at the same time */
+Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
+
+setup(){
+  Serial.begin(9600);
+  
+  if(!mag.begin())
+  {
+    /* There was a problem detecting the HMC5883 ... check your connections */
+    Serial.println("Ooops, no HMC5883 detected ... Check your wiring!");
+  }
+}
+
+loop(){
+  /* Get a new sensor event */ 
+  sensors_event_t event; 
+  mag.getEvent(&event);
+  
+  float heading = atan2(event.magnetic.y, event.magnetic.x);
+  
+  //Declination Angle of Jaraguá do Sul is -0.31 and from Mauá is -0.35
+  float declinationAngle = -0.31;
+  heading += declinationAngle;
+  
+  // Correct for when signs are reversed.
+  if(heading < 0)
+    heading += 2*PI;
+    
+  // Check for wrap due to addition of declination.
+  if(heading > 2*PI)
+    heading -= 2*PI;
+   
+  // Convert radians to degrees for readability.
+  float headingDegrees = heading * 180/M_PI; 
+  
+  if(headingDegrees <= 180)Serial.print("vire a esquerda"); 
+  else Serial.print("vire a direita");
+  
+  delay(500);
+}
